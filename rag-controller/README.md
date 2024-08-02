725aab698bb340e9bf12b748bcafc5c5bf6786bd
clearml-agent <duong05102002@gmail.com>
2024-08-02 10:25:00 +0700

    update local test

---
+++ rag-controller/README.md
@@ -0,0 +1,6 @@
+## Local deployment for testing api
+
+```bash
+docker-compose up --build
+bash start_local.sh
+```
--- rag-controller/build.sh
+++
@@ -1,8 +0,0 @@
-i=12
-
-# docker build -t legal-rag:v1.$i .
-# docker tag legal-rag:v1.$i asia.gcr.io/legal-rag-427109/legal-rag:v1.$i
-# docker push asia.gcr.io/legal-rag-427109/legal-rag:v1.$i
-kubens rag
-helm upgrade --install rag helm-charts/rag --set deployment.image.name=asia.gcr.io/orbital-age-427114-u7/legal-rag \
---set deployment.image.version=v1.$i
